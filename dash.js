0fc62806713a255502e65c3c125904c22c19784b
Nilesh <146858108+Nilesh7781@users.noreply.github.com>
2025-07-11 11:25:48 +0530

    Add files via upload

---
+++ dash.js
@@ -0,0 +1,7 @@
+// // Navigation functionality
+// document.querySelectorAll('.sidebar a').forEach(link => {
+//     link.addEventListener('click', event => {
+//         event.preventDefault();
+//         alert(You clicked on ${event.target.id}!);
+//     });
+// });
---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('accountForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Account created successfully!');
+});
